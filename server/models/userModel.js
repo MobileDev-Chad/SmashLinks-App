50c59dd456a80e08d03d7d7f834248bebed6f2e5
MobileDev-Chad <chadwin.allotey@gmail.com>
2023-01-22 16:19:30 -0500

    [ADDED] Added userModel.js

---
+++ server/models/userModel.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      match: /.+\@.+\..+/,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: [8, "Password must be at least 8 characters long"],
+    },
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    profilePicture: String,
+    coverPicture: String,
+    country: String,
+    followers: [],
+    following: [],
+    favoriteCharacter: { type: Array },
+    wins: Number,
+    losses: Number,
+    // ratio: ,
+  },
+  { timestamps: true }
+);
+
+const UserModel = mongoose.model("Users", UserSchema);
+export default UserModel;
