8e107a843b1c32b647477fc7fb5c6210d9c42b3e
MobileDev-Chad <chadwin.allotey@gmail.com>
2023-01-22 17:33:22 -0500

    [FIX] Included register in authRequest.js api

--- client/src/api/authRequest.js
+++ client/src/api/authRequest.js
@@ -1,7 +1,11 @@
 import client from './client';
 
-const login = (email, password) => client.post('/auth/login', {email, password} );
+const login = (email, password) =>
+  client.post('/auth/login', { email, password });
+
+const register = (userInfo) => client.post('/auth/register', userInfo);
 
 export default {
-    login
-}
+  login,
+  register,
+};
