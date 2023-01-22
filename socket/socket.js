d9872b8e3acee05951a168159eef0abca3890259
MobileDev-Chad <chadwin.allotey@gmail.com>
2023-01-22 16:43:30 -0500

    [TYPO,REMOVE,REFACTOR] Removed express

--- socket/socket.js
+++ socket/socket.js
@@ -1,9 +1,7 @@
-const express = require('express');
-const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-    origins: ['http://192.168.2.11:5000'],
+    origins: ['http://192.168.2.11:3000'],
   },
 });
 
