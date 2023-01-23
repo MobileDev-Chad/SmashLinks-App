836b88c8acb5cf648fda8af96feac7ce9512d277
MobileDev-Chad <chadwin.allotey@gmail.com>
2023-01-22 22:28:31 -0500

    [REMOVED] Removed expo-status-bar

--- client/App.js
+++ client/App.js
@@ -1,12 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useCallback, useState } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { Provider as PaperProvider } from 'react-native-paper';
+
+import { AuthProvider } from './src/auth/context';
+import authStorage from './src/auth/storage';
+
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { navigationRef } from './src/navigation/rootNavigation';
+import AppNavigator from './src/navigation/AppNavigator';
+import AuthNavigator from './src/navigation/AuthNavigator';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  (async () => {
+    const user = await authStorage.getUser();
+    if (user) setCurrentUser(user);
+  })();
+
+  const [fontsLoaded] = useFonts({
+    'Roboto-Regular': require('./src/assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Black': require('./src/assets/fonts/Roboto-Black.ttf'),
+    'Roboto-Bold': require('./src/assets/fonts/Roboto-Bold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthProvider value={{ currentUser }}>
+      <PaperProvider>
+        <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+          <NavigationContainer ref={navigationRef}>
+            {currentUser ? <AppNavigator /> : <AuthNavigator />}
+          </NavigationContainer>
+        </View>
+      </PaperProvider>
+    </AuthProvider>
   );
 }
 
