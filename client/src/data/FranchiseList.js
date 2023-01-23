73f73498da94b566b1c8248ae63bb1ca2d1e8359
MobileDev-Chad <chadwin.allotey@gmail.com>
2023-01-22 22:27:02 -0500

    [ADDED] Added FranchiseList.js

---
+++ client/src/data/FranchiseList.js
@@ -0,0 +1,305 @@
+import { Asset } from "expo-asset";
+
+export const FranchiseList = [
+  {
+    id: "1",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/MarioSymbol.png")
+    ).uri,
+    franchise: "Super Mario",
+  },
+  {
+    id: "2",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/DKSymbol.png")
+    ).uri,
+    franchise: "Donkey Kong",
+  },
+  {
+    id: "3",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/ZeldaSymbol.png")
+    ).uri,
+    franchise: "Zelda",
+  },
+  {
+    id: "4",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/MetroidSymbol.png")
+    ).uri,
+    franchise: "Metroid",
+  },
+
+  {
+    id: "5",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/YoshiSymbol.png")
+    ).uri,
+    franchise: "Yoshi",
+  },
+  {
+    id: "6",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/KirbySymbol.png")
+    ).uri,
+    franchise: "Kirby",
+  },
+  {
+    id: "7",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/StarFoxSymbol.png")
+    ).uri,
+    franchise: "Star Fox",
+  },
+  {
+    id: "8",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/PokemonSymbol.png")
+    ).uri,
+    franchise: "Pokemon",
+  },
+
+  {
+    id: "10",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/EarthboundSymbol.png")
+    ).uri,
+    franchise: "EarthBound",
+  },
+  {
+    id: "11",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/FZeroSymbol.png")
+    ).uri,
+    franchise: "F-Zero",
+  },
+
+  {
+    id: "15",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/IceClimberSymbol.png")
+    ).uri,
+    franchise: "Ice Climbers",
+  },
+
+  {
+    id: "21",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/FireEmblemSymbol.png")
+    ).uri,
+    franchise: "Fire Emblem",
+  },
+  {
+    id: "26",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/GameWatchSymbol.png")
+    ).uri,
+    franchise: "Game & Watch",
+  },
+
+  {
+    id: "28",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/KidIcarusSymbol.png")
+    ).uri,
+    franchise: "Kid Icarus",
+  },
+
+  {
+    id: "30",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/WarioSymbol.png")
+    ).uri,
+    franchise: "Wario Ware",
+  },
+  {
+    id: "31",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/MetalGearSymbol.png")
+    ).uri,
+    franchise: "Metal Gear",
+  },
+
+  {
+    id: "38",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/SonicSymbol.png")
+    ).uri,
+    franchise: "Sonic",
+  },
+
+  {
+    id: "40",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/PikminSymbol.png")
+    ).uri,
+    franchise: "Pikmin",
+  },
+
+  {
+    id: "42",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/ROBSymbol.png")
+    ).uri,
+    franchise: "R.O.B.",
+  },
+
+  {
+    id: "45",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/AnimalCrossingSymbol.png")
+    ).uri,
+    franchise: "Animal Crossing",
+  },
+  {
+    id: "46",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/MegaManSymbol.png")
+    ).uri,
+    franchise: "Mega Man",
+  },
+  {
+    id: "47",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/WiiFitSymbol.png")
+    ).uri,
+    franchise: "Wii Fit",
+  },
+
+  {
+    id: "49",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/PunchOutSymbol.png")
+    ).uri,
+    franchise: "Punch-Out!!",
+  },
+
+  {
+    id: "51",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/SmashBrosSymbol.png")
+    ).uri,
+    franchise: "Ultimate",
+  },
+
+  {
+    id: "55",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/PacManSymbol.png")
+    ).uri,
+    franchise: "Pac-Man",
+  },
+
+  {
+    id: "57",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/XenobladeSymbol.png")
+    ).uri,
+    franchise: "Xenoblade",
+  },
+
+  {
+    id: "59",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/DuckHuntSymbol.png")
+    ).uri,
+    franchise: "Duck Hunt",
+  },
+  {
+    id: "60",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/StreetFighterSymbol.png")
+    ).uri,
+    franchise: "Street Fighter",
+  },
+
+  {
+    id: "61",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/FinalFantasySymbol.png")
+    ).uri,
+    franchise: "Final Fantasy",
+  },
+  {
+    id: "63",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/BayonettaSymbol.png")
+    ).uri,
+    franchise: "Bayonetta",
+  },
+  {
+    id: "63",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/SplatoonSymbol.png")
+    ).uri,
+    franchise: "Splatoon",
+  },
+  {
+    id: "066",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/CastlevaniaSymbol.png")
+    ).uri,
+    franchise: "Castlevania",
+  },
+
+  {
+    id: "71",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/PersonaSymbol.png")
+    ).uri,
+    franchise: "Persona",
+  },
+  {
+    id: "72",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/DragonQuestSymbol.png")
+    ).uri,
+    franchise: "Dragon Quest",
+  },
+  {
+    id: "73",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/BanjoKazooieSymbol.png")
+    ).uri,
+    franchise: "Banjo-Kazooie",
+  },
+  {
+    id: "74",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/FatalFurySymbol.png")
+    ).uri,
+    franchise: "Fatal Fury",
+  },
+
+  {
+    id: "76",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/ARMSSymbol.png")
+    ).uri,
+    franchise: "ARMS",
+  },
+  {
+    id: "77",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/MinecraftSymbol.png")
+    ).uri,
+    franchise: "MINECRAFT",
+  },
+
+  {
+    id: "81",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/TekkenSymbol.png")
+    ).uri,
+    franchise: "Tekken",
+  },
+  {
+    id: "82",
+    icon: Asset.fromModule(
+      require("../assets/icons/franchise_icons/KingdomHeartsSymbol.png")
+    ).uri,
+    franchise: "Kingdom Hearts",
+  },
+];
+
+export default FranchiseList;
