f3cec5d2df3bb7a2251c8acb881dc660104ed8bc
MobileDev-Chad <chadwin.allotey@gmail.com>
2023-01-27 20:14:42 -0500

    [ADDED] Added a server message

--- server/server.js
+++ server/server.js
@@ -3,13 +3,12 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 
 // Routes
 import AuthRoute from "./routes/AuthRoute.js";
 import UserRoute from "./routes/UserRoute.js";
 import PostRoute from "./routes/PostRoute.js";
-
 
 const app = express();
 
@@ -17,29 +16,30 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-
+app.use(cookieParser());
 
 mongoose.set("strictQuery", true);
 
 dotenv.config();
 const PORT = process.env.PORT;
 
-const CONNECTION =process.env.MONGO_DB;
+const CONNECTION = process.env.MONGO_DB;
 
 mongoose
   .connect(CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Listening at PORT: ${PORT}`)
-    )
-  )
+  .then(() => app.listen(PORT, () => console.log(`Listening at PORT: ${PORT}`)))
   .catch((error) => console.log(error));
 
-// Usage of 
+// Usage of
 
 app.use("/auth", AuthRoute);
 app.use("/user", UserRoute);
 app.use("/posts", PostRoute);
+
+// Server connection message
+app.get("/", (req, res) => {
+  res.send("Server is working!");
+});
