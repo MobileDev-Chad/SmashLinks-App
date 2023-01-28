369af51c904e2c9713d82daa2ebcc283999ef76a
MobileDev-Chad <chadwin.allotey@gmail.com>
2023-01-27 23:45:59 -0500

    [ADDED] Installed localtunnel

--- server/server.js
+++ server/server.js
@@ -13,9 +13,9 @@ import PostRoute from "./routes/PostRoute.js";
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
 app.use(cookieParser());
 
 mongoose.set("strictQuery", true);
