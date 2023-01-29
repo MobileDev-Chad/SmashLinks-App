995241e2a93d87ebc34a082b067a6e174507ef20
MobileDev-Chad <chadwin.allotey@gmail.com>
2023-01-28 22:23:04 -0500

    [UPDATE] Formatted client.js

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -8,10 +8,10 @@ const apiClient = create({
 });
 
 apiClient.addAsyncRequestTransform(async (request) => {
-const authToken = await authStorage.getToken();
-if (!authToken) return;
-request.headers["x-auth-token"] = authToken;
-})
+  const authToken = await authStorage.getToken();
+  if (!authToken) return;
+  request.headers['x-auth-token'] = authToken;
+});
 
 const get = apiClient.get;
 apiClient.get = async (url, params, axiosConfig) => {
@@ -22,8 +22,8 @@ apiClient.get = async (url, params, axiosConfig) => {
     return response;
   }
 
-  const data = await cache.get(url)
-  return data ? {ok: true, data} : response;
+  const data = await cache.get(url);
+  return data ? { ok: true, data } : response;
 };
 
 export default apiClient;
