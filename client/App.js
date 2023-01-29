2d2424885734fbc4560dc8060198fb4cfbf84a56
MobileDev-Chad <chadwin.allotey@gmail.com>
2023-01-28 22:15:06 -0500

    [UPDATE] Incorporated redux provider

--- client/App.js
+++ client/App.js
@@ -1,61 +1,16 @@
-import React, { useCallback, useState } from 'react';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import { Provider as PaperProvider } from 'react-native-paper';
+import Main from './Main';
 
-import { AuthProvider } from './src/auth/context';
-import authStorage from './src/auth/storage';
-
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { navigationRef } from './src/navigation/rootNavigation';
-import AppNavigator from './src/navigation/AppNavigator';
-import AuthNavigator from './src/navigation/AuthNavigator';
-
-SplashScreen.preventAutoHideAsync();
+// import store and provider
+import { persistor, store } from './src/redux/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 export default function App() {
-  const [currentUser, setCurrentUser] = useState(null);
-
-  (async () => {
-    const user = await authStorage.getUser();
-    if (user) setCurrentUser(user);
-  })();
-
-  const [fontsLoaded] = useFonts({
-    'Roboto-Regular': require('./src/assets/fonts/Roboto-Regular.ttf'),
-    'Roboto-Black': require('./src/assets/fonts/Roboto-Black.ttf'),
-    'Roboto-Bold': require('./src/assets/fonts/Roboto-Bold.ttf'),
-  });
-
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-
-  if (!fontsLoaded) {
-    return null;
-  }
-
   return (
-    <AuthProvider value={{ currentUser }}>
-      <PaperProvider>
-        <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
-          <NavigationContainer ref={navigationRef}>
-            {currentUser ? <AppNavigator /> : <AuthNavigator />}
-          </NavigationContainer>
-        </View>
-      </PaperProvider>
-    </AuthProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Main />
+      </PersistGate>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
