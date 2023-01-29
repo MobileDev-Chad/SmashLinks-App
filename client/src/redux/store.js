b188880f15df731a2a90d4eaba26d1359c82797e
MobileDev-Chad <chadwin.allotey@gmail.com>
2023-01-28 22:16:27 -0500

    [ADDED] Added redux store

---
+++ client/src/redux/store.js
@@ -0,0 +1,40 @@
+import { configureStore } from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import authReducer from './slices/authSlice';
+import messageReducer from './slices/messageSlice';
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  authReducer,
+  messageReducer
+);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: true,
+});
+
+export const persistor = persistStore(store);
